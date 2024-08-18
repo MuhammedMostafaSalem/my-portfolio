f572b6ed618af6c77b4725867d8c4a8ca8cde9a5
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-08-18 12:53:18 +0300

    Add screens and (colors & boxShadow extend)

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      "sm1-min": { min: "800px" },
+    },
+    extend: {
+      colors: {
+        'primary': '#9747FF',
+        'textNeutralGray': '#888888',
+        'textBlack': '#1A1D1F',
+        'textWhaite': '#FFFFFF',
+        'bgWhaite': '#FFFFFF',
+        'bgblack': '#1f2128',
+        'bgCharcoalGray': '#242731',
+        'bgSoftGray': '#F5F4F6',
+      },
+      boxShadow: {
+        'custom': '0 4px 4px rgba(0, 0, 0, 0.25)',
+      }
+    },
   },
   plugins: [],
 }
