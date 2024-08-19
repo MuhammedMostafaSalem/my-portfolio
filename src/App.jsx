59ef26ae01a7d4fce184efe414b9d46e517b3507
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-08-19 09:31:55 +0300

    Add Projects section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Hero from "./sections/hero/Hero"
+import Projects from "./sections/projects/Projects"
 
 function App() {
 
   return (
     <div>
       <Hero />
+      <Projects />
     </div>
   )
 }
