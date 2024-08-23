dccee34efc7da8d5c82fe54c9c7e059ba215a6d8
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-08-23 19:43:59 +0300

    Add darkModeReducer slice

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import darkModeReducer from './reducer/darkModeReducer'
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        darkMode: darkModeReducer
+    },
 })
