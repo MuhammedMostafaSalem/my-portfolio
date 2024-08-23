56666a6911c12b3cdbacb2f0d209eac945b2cfa3
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-08-23 17:19:13 +0300

    Add Skills section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from "./sections/hero/Hero"
 import Projects from "./sections/projects/Projects"
+import Skills from "./sections/skills/Skills"
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Hero />
       <Projects />
+      <Skills />
     </div>
   )
 }
