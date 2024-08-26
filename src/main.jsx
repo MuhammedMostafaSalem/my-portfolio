4de803d37e453a47b8d09022f967ee186254c58c
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-08-26 16:48:18 +0300

    Add ToastContainer

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,26 @@ import { Provider } from 'react-redux'
 import { store } from './store/store.js'
 import { I18nextProvider } from 'react-i18next'
 import i18n from './i18n.js'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <I18nextProvider i18n={i18n}>
         <App />
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
       </I18nextProvider>
     </Provider>
   </StrictMode>,
