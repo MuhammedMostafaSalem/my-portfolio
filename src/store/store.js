f12f2c9f733984c7a3f530ccc53fca163df609a6
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-08-26 16:47:08 +0300

    Add contactReducer slice

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import darkModeReducer from './reducer/darkModeReducer'
 import languageReducer from './reducer/languageReducer'
+import contactReducer from './reducer/contactReducer'
 
 export const store = configureStore({
     reducer: {
         darkMode: darkModeReducer,
-        lang: languageReducer
+        lang: languageReducer,
+        contact: contactReducer
     },
 })
