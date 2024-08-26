3607202838b7de597a0f34a937830e90c0c830a0
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-08-26 14:58:04 +0300

    Add new projects imgs

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,7 +3,7 @@ import { MdLanguage } from "react-icons/md";
 import { FaGithub } from "react-icons/fa6";
 import { useSelector } from 'react-redux';
 
-const ProjectCard = ({filteredItems}) => {
+const ProjectCard = ({filteredItems, siteNewTap}) => {
     const { isDarkMode } = useSelector(state => state.darkMode);
 
     return (
@@ -16,22 +16,27 @@ const ProjectCard = ({filteredItems}) => {
                     >
                         <img
                             className="max-w-[250px] self-center rounded-[20px]"
-                            src='https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80'
+                            src={item.img}
                             alt=''
                         />
-                        <h3>Name Project</h3>
-                        <p>Type Project</p>
+                        <h3>{item.title}</h3>
+                        <p>{item.type}</p>
 
                         <div className='flex justify-center mt-[5px] text-[20px] gap-4 text-textNeutralGray'>
-                            <MdLanguage className='cursor-point
-                                transition
-                                ease-in-out
-                                delay-150
-                                hover:-translate-y-1
-                                hover:scale-110
-                                hover:text-primary
-                                duration-300'
-                            />
+                            {
+                                item.site ?
+                                    <MdLanguage className='cursor-point
+                                        transition
+                                        ease-in-out
+                                        delay-150
+                                        hover:-translate-y-1
+                                        hover:scale-110
+                                        hover:text-primary
+                                        duration-300'
+                                        onClick={() => siteNewTap(item.site)}
+                                    />
+                                : null
+                            }
                             <FaGithub className='cursor-point
                                 transition
                                 ease-in-out
@@ -40,6 +45,7 @@ const ProjectCard = ({filteredItems}) => {
                                 hover:scale-110
                                 hover:text-primary
                                 duration-300'
+                                onClick={() => siteNewTap(item.github)}
                             />
                         </div>
                     </div>
