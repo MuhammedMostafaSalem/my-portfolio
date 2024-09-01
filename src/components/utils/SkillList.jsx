6e8a0e720ec9c30325ead6c28d700a18914e27f0
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-09-01 23:41:13 +0300

    Remove degree

--- src/components/utils/SkillList.jsx
+++ src/components/utils/SkillList.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { GiCheckMark } from "react-icons/gi";
 
-const SkillList = ({ skill, degree, isDarkMode }) => {
+const SkillList = ({ skill, isDarkMode }) => {
 
     return (
         <div className={`flex gap-2 items-center ${isDarkMode ? 'text-textWhaite' : 'text-textBlack'}`}>
             <GiCheckMark />
             <p>{skill}</p>
-            <p>{degree}</p>
         </div>
     )
 }
--- src/sections/skills/Skills.jsx
+++ src/sections/skills/Skills.jsx
@@ -13,27 +13,27 @@ const Skills = () => {
             <Title title={t("Skills")} />
 
             <div className='flex justify-center self-center flex-wrap gap-x-[40px] max-w-[26ch] sm1-min:max-w-fit sm1-min:gap-[30px]'>
-                <SkillList skill="HTML" degree="100%" isDarkMode={isDarkMode} />
-                <SkillList skill="CSS" degree="95%" isDarkMode={isDarkMode} />
-                <SkillList skill="JavaScript" degree="70%" isDarkMode={isDarkMode} />
+                <SkillList skill="HTML" isDarkMode={isDarkMode} />
+                <SkillList skill="CSS" isDarkMode={isDarkMode} />
+                <SkillList skill="JavaScript" isDarkMode={isDarkMode} />
             </div>
 
             <hr className={`my-1 border-1 ${isDarkMode ? 'border-bgWhaite' : 'border-bgblack'} w-[100px] sm1-min:w-[300px] mx-auto`} />
 
             <div className='flex justify-center self-center flex-wrap gap-x-[40px] max-w-[26ch] sm1-min:max-w-fit sm1-min:gap-[30px]'>
-                <SkillList skill="react.js" degree="100%" isDarkMode={isDarkMode} />
-                <SkillList skill="redux" degree="100%" isDarkMode={isDarkMode} />
-                <SkillList skill="redux-toolkit" degree="100%" isDarkMode={isDarkMode} />
-                <SkillList skill="node.js" degree="30%" isDarkMode={isDarkMode} />
+                <SkillList skill="react.js" isDarkMode={isDarkMode} />
+                <SkillList skill="redux" isDarkMode={isDarkMode} />
+                <SkillList skill="redux-toolkit" isDarkMode={isDarkMode} />
+                <SkillList skill="node.js" isDarkMode={isDarkMode} />
             </div>
 
             <hr className={`my-1 border-1 ${isDarkMode ? 'border-bgWhaite' : 'border-bgblack'} w-[100px] sm1-min:w-[300px] mx-auto`} />
 
             <div className='flex justify-center self-center flex-wrap gap-x-[40px] max-w-[26ch] sm1-min:max-w-fit sm1-min:gap-[30px]'>
-                <SkillList skill="bootstrap" degree="100%" isDarkMode={isDarkMode} />
-                <SkillList skill="react-bootstrap" degree="100%" isDarkMode={isDarkMode} />
-                <SkillList skill="tailwindcss" degree="100%" isDarkMode={isDarkMode} />
-                <SkillList skill="material-tailwind" degree="100%" isDarkMode={isDarkMode} />
+                <SkillList skill="bootstrap" isDarkMode={isDarkMode} />
+                <SkillList skill="react-bootstrap" isDarkMode={isDarkMode} />
+                <SkillList skill="tailwindcss" isDarkMode={isDarkMode} />
+                <SkillList skill="material-tailwind" isDarkMode={isDarkMode} />
             </div>
         </div>
     )
