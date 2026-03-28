269ff8b6a01dbad6db306deea04959589856bf1f
Marwan Salem <marwansalem164@gmail.com>
2026-03-28 08:57:09 +0200

    Update in projects card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
 import { MdLanguage } from "react-icons/md";
 import { FaGithub } from "react-icons/fa6";
 import { useSelector } from 'react-redux';
 
-const ProjectCard = ({filteredItems, siteNewTap}) => {
+const ProjectCard = ({ filteredItems, siteNewTap }) => {
     const { isDarkMode } = useSelector(state => state.darkMode);
 
     return (
@@ -12,16 +11,40 @@ const ProjectCard = ({filteredItems, siteNewTap}) => {
                 filteredItems.map(item => (
                     <div
                         key={item.id}
-                        className={`w-fit ${isDarkMode ? 'text-textWhaite' : 'text-textBlack'}`}
+                        className={`relative w-fit ${isDarkMode ? 'text-textWhaite' : 'text-textBlack'} flex flex-col gap-2`}
                     >
+                        <span className={`absolute top-0 left-0 text-[8px] px-2 py-1 rounded w-fit text-white
+                            ${item.status === "Completed" ? "bg-green-600" : "bg-yellow-500"}`}>
+                            {item.status}
+                        </span>
+                        {/* Image */}
                         <img
                             className="max-w-[250px] self-center rounded-[20px]"
                             src={item.img}
                             alt=''
                         />
+                        {/* Content */}
                         <h3>{item.title}</h3>
+                        <p className="max-w-[30ch] mx-auto text-[13px] text-gray-400">
+                            {item.description}
+                        </p>
                         <p>{item.type}</p>
 
+                        {/* Tech Stack */}
+                        <div className="flex justify-center flex-wrap gap-[10px] max-w-[30ch]">
+                            {
+                                item.tech?.map((tech, index) => (
+                                    <span
+                                        key={index}
+                                        className="text-xs text-primary"
+                                    >
+                                        {tech}
+                                    </span>
+                                ))
+                            }
+                        </div>
+
+                        {/* Links */}
                         <div className='flex justify-center mt-[5px] text-[20px] gap-4 text-textNeutralGray'>
                             {
                                 item.site ?
@@ -35,7 +58,7 @@ const ProjectCard = ({filteredItems, siteNewTap}) => {
                                         duration-300'
                                         onClick={() => siteNewTap(item.site)}
                                     />
-                                : null
+                                    : null
                             }
                             <FaGithub className='cursor-point
                                 transition
